d08bce69247c8fba917b7e40a5aef30388bbeb1d
Aidan Smith <aidansm2103@gmail.com>
2026-02-10 11:50:55 +0200

    Made case domain model.

---
+++ Domain/Case.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evidence_Locker.Domain
+{
+    public enum CaseStatus
+    {
+        Cold,
+        Reopened,
+        Closed
+    }
+    internal class Case
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public CaseStatus Status { get; set; }
+
+        public Case(int id, string title, string summary, CaseStatus status)
+        {
+            Id = id;
+            Title = title;
+            Summary = summary;
+            Status = status;
+        }
+
+        public void Close()
+        {
+            Status = CaseStatus.Closed;
+        }
+
+        public void Reopen()
+        {
+            Status = CaseStatus.Reopened;
+        }
+    }
+}
