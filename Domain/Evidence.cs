b9d95268cc3d9e5ff788b99a6edd64b40d2e4550
Aidan Smith <aidansm2103@gmail.com>
2026-02-11 17:00:42 +0200

    Added evidence class to Domain.

---
+++ Domain/Evidence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evidence_Locker.Domain
+{
+    public enum EvidenceType
+    {
+        WitnessStatement,
+        ForensicReport,
+        PhysicalItem
+    }
+    internal class Evidence
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public EvidenceType Type { get; set; }
+        public int CaseId { get; set; }
+        public DateTime DateCollected { get; set; }
+        public Evidence(int id, string description, EvidenceType type, int caseId, DateTime dateCollected)
+        {
+            Id = id;
+            Description = description;
+            Type = type;
+            CaseId = caseId;
+            DateCollected = dateCollected;
+        }
+    }
+}
