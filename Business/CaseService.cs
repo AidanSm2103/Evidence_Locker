1c679ea9c9ca547725ebd403bffc33a179d0611a
Aidan Smith <aidansm2103@gmail.com>
2026-02-12 10:50:21 +0200

    Created business layer and added functions to add a case, close a case, reopen a case and to find a case by its ID.

---
+++ Business/CaseService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evidence_Locker.Domain;
+
+namespace Evidence_Locker.Business
+{
+    internal class CaseService
+    {
+        private List<Case> cases = new List<Case>();
+        public void AddCase(Case newCase)
+        {
+            cases.Add(newCase);
+        }
+        public Case GetCaseById(int id)
+        {
+            return cases.FirstOrDefault(c => c.Id == id);
+        }
+        public List<Case> GetAllCases()
+        {
+            return cases;
+        }
+        public void CloseCase(int id)
+        {
+            var caseToClose = GetCaseById(id);
+            if (caseToClose != null)
+            {
+                caseToClose.Close();
+            }
+        }
+        public void ReopenCase(int id)
+            {
+                var caseToReopen = GetCaseById(id);
+                if (caseToReopen != null)
+                {
+                    caseToReopen.Reopen();
+                }
+        }
+    }
+}
